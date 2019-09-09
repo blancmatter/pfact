a93f35a9b30d05f3982185d8020bed96b2d9d546
Doug Arnold <d.m.arnold@ljmu.ac.uk>
2019-09-09 17:43:33 +0100

    added list of transients for last 12 months

--- getTNS.py
+++ getTNS.py
@@ -4,6 +4,10 @@ import os, sys
 
 command = 'wget -O PAGE.csv "https://wis-tns.weizmann.ac.il/search?&page=PAGE&num_page=1000&discovered_period_value=12&discovered_period_units=months&format=csv"'
 
-for i in range(1,50):
-    print(command.replace('PAGE', str(i)))
-    os.system(command.replace('PAGE', str(i)))
+for i in range(1,16):
+    #print(command.replace('PAGE', str(i)))
+    #os.system(command.replace('PAGE', str(i)))
+
+    command = 'tail -n +1 ' + str(i) + '.csv >> full.csv'
+    print (command)
+    os.system(command)
